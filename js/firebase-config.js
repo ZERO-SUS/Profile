917d24e315f775776b7c6a0bc4c8d216f62af3c2
ZERO SUS <suszero9@gmail.com>
2026-08-07 22:30:51 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDTCGGeI1HA-yfOyTI8CO10BTtbneM0oOs",
-  authDomain: "zero-sus.firebaseapp.com",
-  projectId: "zero-sus",
-  storageBucket: "zero-sus.firebasestorage.app",
-  messagingSenderId: "308451617845",
-  appId: "1:308451617845:web:92b8667a9f4f57e28c957c"
+  apiKey: "AIzaSyDJgbFuv9yr_r7DivUU15y9uvFdvSimsnQ",
+  authDomain: "zero-sus-f57c1.firebaseapp.com",
+  projectId: "zero-sus-f57c1",
+  storageBucket: "zero-sus-f57c1.firebasestorage.app",
+  messagingSenderId: "660707509071",
+  appId: "1:660707509071:web:936bda9657d1d45011bb3a",
+  measurementId: "G-TNZD5YJP8H"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
